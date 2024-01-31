36ef6662c74c36a68a2d283272e547ea71eebaf3
Joshua D Schoep <joshuaschoep@gmail.com>
2024-01-31 12:34:54 -0700

    pt 4

--- .github/scripts/lambda.sh
+++ .github/scripts/lambda.sh
@@ -1,1 +1,1 @@
-aws lambda update-function-code --function-name=arn:aws:lambda:us-east-1:145724753995:function:authorizeSchoepprojectDevelopmentUsers --zip-file=build.zip 
+aws lambda update-function-code --function-name=arn:aws:lambda:us-east-1:145724753995:function:authorizeSchoepprojectDevelopmentUsers --zip-file=fileb://build.zip 
