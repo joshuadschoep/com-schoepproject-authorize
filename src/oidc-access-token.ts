71e6fcabe274d32cc7e9a79cde93b5aae6362ebf
Joshua D Schoep <joshuaschoep@gmail.com>
2024-02-12 10:46:54 -0700

    fix scope validation

--- src/oidc-access-token.ts
+++ src/oidc-access-token.ts
@@ -8,19 +8,23 @@ export class JWTScopeValidationFailed extends Error {
   }
 }
 
+interface JWTScope {
+  scope: string
+}
+
 export const validateToken = async (accessToken: any) => {
   const JWKS = jose.createRemoteJWKSet(
     new URL(`${process.env.OAUTH_ISSUER}.well-known/jwks.json`)
   );
 
-  const { payload } = await jose.jwtVerify(accessToken, JWKS, {
+  const { payload } = await jose.jwtVerify<JWTScope>(accessToken, JWKS, {
     issuer: process.env.OAUTH_ISSUER ?? "unclaimable_issuer",
     audience: process.env.OAUTH_AUDIENCE ?? "unclaimable_audience",
     algorithms: [process.env.OAUTH_ALGORITHM as Algorithm],
   });
 
   if (
-    payload.scope === (process.env.OAUTH_REQUIRED_CLAIM ?? "unclaimable:scope")
+    payload.scope.includes(process.env.OAUTH_REQUIRED_CLAIM ?? "unclaimable:scope")
   ) {
     return payload;
   } else {
